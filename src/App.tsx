c7dee4f5e637195286ae557c70e0346a88ec0c6b
gooxpf <gooxpf@gmail.com>
2022-08-10 09:19:47 +0400

    [INIT] init vite react eslint

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react';
+
+import './App.css';
+import reactLogo from './assets/react.svg';
+
+function App(): ReactElement {
+  return (
+    <div>
+      <h1>Vite React</h1>
+      <img src={reactLogo} alt="react-logotype" />
+    </div>
+  );
+}
+
+export default App;
